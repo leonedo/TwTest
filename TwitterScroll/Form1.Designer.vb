0683b44c404106b64c5e10942140bab8ab318617
Leon Hurtado <leonedo@gmail.com>
2016-05-27 13:24:42 -0400

    todo? es asi..

---
+++ TwitterScroll/Form1.Designer.vb
@@ -0,0 +1,175 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.dgvtwitter = New System.Windows.Forms.DataGridView()
+        Me.Column3 = New System.Windows.Forms.DataGridViewCheckBoxColumn()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.TextBoxTw = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        CType(Me.dgvtwitter, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Location = New System.Drawing.Point(542, 564)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Load"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'dgvtwitter
+        '
+        Me.dgvtwitter.AllowUserToAddRows = False
+        Me.dgvtwitter.AllowUserToDeleteRows = False
+        Me.dgvtwitter.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.dgvtwitter.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.dgvtwitter.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvtwitter.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column3, Me.Column1, Me.Column4, Me.Column2, Me.Column5})
+        Me.dgvtwitter.Location = New System.Drawing.Point(12, 12)
+        Me.dgvtwitter.Name = "dgvtwitter"
+        Me.dgvtwitter.RowHeadersVisible = False
+        Me.dgvtwitter.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dgvtwitter.Size = New System.Drawing.Size(799, 542)
+        Me.dgvtwitter.TabIndex = 1
+        '
+        'Column3
+        '
+        Me.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None
+        Me.Column3.FillWeight = 50.0!
+        Me.Column3.HeaderText = "Show"
+        Me.Column3.Name = "Column3"
+        Me.Column3.Resizable = System.Windows.Forms.DataGridViewTriState.[False]
+        Me.Column3.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic
+        Me.Column3.Width = 50
+        '
+        'Column1
+        '
+        Me.Column1.FillWeight = 80.0!
+        Me.Column1.HeaderText = "User"
+        Me.Column1.MinimumWidth = 120
+        Me.Column1.Name = "Column1"
+        '
+        'Column4
+        '
+        Me.Column4.FillWeight = 130.0!
+        Me.Column4.HeaderText = "User Name"
+        Me.Column4.MinimumWidth = 130
+        Me.Column4.Name = "Column4"
+        '
+        'Column2
+        '
+        Me.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill
+        Me.Column2.FillWeight = 1000.0!
+        Me.Column2.HeaderText = "Text"
+        Me.Column2.MinimumWidth = 10
+        Me.Column2.Name = "Column2"
+        '
+        'Column5
+        '
+        Me.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None
+        Me.Column5.FillWeight = 150.0!
+        Me.Column5.HeaderText = "Date"
+        Me.Column5.Name = "Column5"
+        Me.Column5.Width = 150
+        '
+        'TextBoxTw
+        '
+        Me.TextBoxTw.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.TextBoxTw.Location = New System.Drawing.Point(12, 566)
+        Me.TextBoxTw.Name = "TextBoxTw"
+        Me.TextBoxTw.Size = New System.Drawing.Size(138, 20)
+        Me.TextBoxTw.TabIndex = 2
+        Me.TextBoxTw.Text = "#hashtag"
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Button2.Location = New System.Drawing.Point(156, 564)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Search"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button3.Location = New System.Drawing.Point(704, 564)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(107, 23)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Show/Hide"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button4.Location = New System.Drawing.Point(623, 564)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 5
+        Me.Button4.Text = "Kill"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(823, 595)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBoxTw)
+        Me.Controls.Add(Me.dgvtwitter)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "TvTwitterScroll by Leon Hurtado"
+        CType(Me.dgvtwitter, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents dgvtwitter As DataGridView
+    Friend WithEvents TextBoxTw As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Column5 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewCheckBoxColumn
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+End Class
---
+++ TwitterScroll/Form1.vb
@@ -0,0 +1,40 @@
+﻿Imports TweetSharp
+
+Public Class Form1
+    Private Const key1 = "7uoUF6acRAxRWwWSdPLDQU43q"
+    Private Const key2 = "nDzWSBqkGsDR3k8xYp1dRjkOQgiLl8MVCaEhAWa1SGxdwa0DVu"
+    Private Const key3 = "47807160-wu8AW5Azab1IuA6ZWa8Lq67lBYGDT335kuyK0FNCI"
+    Private Const key4 = "ELsIuzduosFiW9IsjpStQwT6PLLceOs39IxFzWJ54YPyC"
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            Dim service As New TwitterService(key1, key2)
+            service.AuthenticateWith(key3, key4)
+            Dim SearchOptions As TweetSharp.SearchOptions = New TweetSharp.SearchOptions() With {.Q = TextBoxTw.Text & " exclude:retweets", .Count = "50", .IncludeEntities = False}
+            Dim Tweets As New TweetSharp.TwitterSearchResult
+            Tweets = service.Search(SearchOptions)
+
+            Dim itwitter As Integer = 0
+            dgvtwitter.Rows.Clear()
+
+            dgvtwitter.Columns(1).DefaultCellStyle.WrapMode = DataGridViewTriState.True
+            For Each tweet As TweetSharp.TwitterStatus In Tweets.Statuses
+
+                dgvtwitter.Rows.Add()
+
+                dgvtwitter.Rows(itwitter).Cells(1).Value = tweet.User.ScreenName
+                dgvtwitter.Rows(itwitter).Cells(2).Value = tweet.User.Name
+                dgvtwitter.Rows(itwitter).Cells(3).Value = tweet.Text
+                dgvtwitter.Rows(itwitter).Cells(4).Value = tweet.CreatedDate.ToLocalTime
+
+                itwitter = itwitter + 1
+            Next
+        Catch ex As Exception
+            MsgBox("BotonCargarTW: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
